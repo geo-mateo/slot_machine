cb691942a53cd5b8af2d85e663b1be4ef4d036e3
geo-mateo <mateusz.kaczmarek32@gmaial.com>
2023-01-06 14:47:09 +0100

    depo lines bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 def deposit():
     while True:
         amount = input("What would you like to deposit? PLN \n")
@@ -25,8 +27,30 @@ def get_number_of_lines():
             print("Please enter a number.")
     
     return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? \n")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance,lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount. Your current balance is: {balance}PLN")
+        else:
+            break
+    print(f"You are betting {bet}PLN on {lines} lines. Total bet is equal to: {total_bet}PLN")
 main()
